2b2f6debc91b94101944ccdb16a5f6cc12878ac9
titus671 <titus.friesen@streamitnet.com>
2022-04-21 07:50:20 -0700

    changed the custom code in sitemonitor.inc.php

--- includes/discovery/sensors/voltage/sitemonitor.inc.php
+++ includes/discovery/sensors/voltage/sitemonitor.inc.php
@@ -45,7 +45,7 @@ $index++;
  * This section of code was added by Stream IT Networks
  * to add support for the functionality of the Packetflux
  * Sitemonitor Expansion modules\
- */
+ *
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.6';
 $desc_oid = '.1.3.6.1.4.1.32050.2.1.27.2.6';
 $index_oid = '.1.3.6.1.4.1.32050.2.1.27.1.6';
@@ -77,3 +77,83 @@ if ($expansion_module == "TriStarMPPTChargeModeRevH"){
     }
   }
 }
+*/
+
+$expansion_module = snmp_get($device, '.1.3.6.1.4.1.32050.2.1.25.2.1', "-Oqv");
+
+switch ($expansion_module) {
+
+  case "TriStarMPPTChargeModeRevH":
+
+    //
+    //  Sensor type mapping and divisor for the TriStar MPPT
+    //  https://docs.librenms.org/Developing/os/Health-Information/#sensors
+    //
+    $sensors = (object) [
+
+      "0" => ["temperature", 10],
+      "1" => ["current", 10],
+      "2" => ["voltage", 10],
+      "3" => ["voltage", 10],
+      "4" => ["current", 10],
+      "5" => ["temperature", 10],
+      "6" => ["temperature", 10],
+      "7" => ["voltage", 100],
+      "8" => ["voltage", 100],
+      "9" => ["voltage", 100],
+      "10" => ["voltage", 100],
+      "11" => ["current", 100],
+      "12" => ["current", 100],
+      "13" => ["temperature", 1],
+      "14" => ["temperature", 1],
+      "15" => ["temperature", 1],
+      "16" => ["voltage", 100],
+      "17" => ["current", 100],
+      "18" => ["count", 1],
+      "19" => ["count", 1],
+      "20" => ["count", 1],
+      "21" => ["count", 1],
+      "22" => ["voltage", 100],
+      "23" => ["current", 10],
+      "24" => ["current", 10],
+      "25" => ["power_consumed", 1],
+      "26" => ["power_consumed", 1],
+      "27" => ["power", 100],
+      "28" => ["power", 100],
+      "29" => ["power", 100],
+      "30" => ["voltage", 100],
+      "31" => ["voltage", 100],
+      "32" => ["voltage", 100],
+      "33" => ["voltage", 100],
+      "34" => ["voltage", 100],
+      "35" => ["count", 1],
+      "36" => ["count", 1],
+      "37" => ["voltage", 100],
+      "38" => ["voltage", 100]
+    ];
+
+    $base_oid = "'.1.3.6.1.4.1.32050.2.1.27.";
+    $idx_index = "1";
+    $desc_index = "2";
+    $value_index = "5";
+
+    // hard coded to 38 sensors that are common with the TriStar MPPT
+    for ($idx=0;$idx<39;$idx++) {
+
+      $index = snmp_get($device, $base_oid.$idx_index.$idx, "-0qv");
+      $desc = snmp_get($device, $base_oid.$desc_index.$idx, "-0qv");
+      $value = snmp_get($device, $base_oid.$value_index.$idx, "-0qv");
+
+      discover_sensor($valid['sensor'], $sensors->$index[0], $device,
+        $base_oid.$value_index.$idx, $idx, 'sitemonitor', $desc,
+        $sensors->$index[1], 1, null, null, null, null, $value);
+    }
+
+    break;
+
+  case "6Voltmeter":
+
+    // TODO
+    break;
+}
+
