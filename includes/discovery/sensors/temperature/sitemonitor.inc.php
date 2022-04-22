ad773edbbce3f57fc1a6776955e73fb423823e9f
Trendal Toews <trendal.toews@gmail.com>
2022-04-21 23:15:14 -0700

    added temperature/sitemonitor support for TriStar MPPT

--- includes/discovery/sensors/temperature/sitemonitor.inc.php
+++ includes/discovery/sensors/temperature/sitemonitor.inc.php
@@ -21,15 +21,68 @@
  *
  * @copyright  2017 Neil Lathwood
  * @author     Neil Lathwood <gh+n@laf.io>
+ *
+ * original code is moved to the default case switch
  */
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.0';
 $current = (snmp_get($device, $oid, '-Oqv') / 10);
-discover_sensor($valid['sensor'], 'temperature', $device, $oid, 0, 'sitemonitor', 'Temperature', 10, 1, null, null, null, null, $current);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.0', '-Oqv'));
+discover_sensor($valid['sensor'], 'temperature', $device, $oid, 0, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
 
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.5';
 $current = (snmp_get($device, $oid, '-Oqv') / 10);
-discover_sensor($valid['sensor'], 'temperature', $device, $oid, 5, 'sitemonitor', 'Relay on Above', 10, 1, null, null, null, null, $current);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.5', '-Oqv'));
+discover_sensor($valid['sensor'], 'temperature', $device, $oid, 5, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
 
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.6';
 $current = (snmp_get($device, $oid, '-Oqv') / 10);
-discover_sensor($valid['sensor'], 'temperature', $device, $oid, 6, 'sitemonitor', 'Relay on Below', 10, 1, null, null, null, null, $current);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.6', '-Oqv'));
+discover_sensor($valid['sensor'], 'temperature', $device, $oid, 6, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
+
+
+
+// Get the expansion unit description, if it exists, if not TODO
+$expansion_module = snmp_get($device, '.1.3.6.1.4.1.32050.2.1.25.2.1', "-Oqv");
+
+switch ($expansion_module) {
+
+  // Run discovery of Tri Star MPPT Charge Controller
+  case "TriStarMPPTChargeModeRevH":
+
+    $sensors = (object) [
+
+      // divisor mapping
+      "0" => 10,
+      "5" => 10,
+      "6" => 10,
+      "13" => 1,
+      "14" => 1,
+      "15" => 1
+    ];
+
+    $base_oid = ".1.3.6.1.4.1.32050.2.1.27.";
+    $idx_index = "1.";
+    $desc_index = "2.";
+    $value_index = "5.";
+
+    // $idx will be the sensor index on the Packetflux
+    foreach ($sensors as $idx => $arr) {
+
+      $index = snmp_get($device, $base_oid.$idx_index.$idx, "-Oqv");
+      $desc = snmp_get($device, $base_oid.$desc_index.$idx, "-Oqv");
+      $value = snmp_get($device, $base_oid.$value_index.$idx, "-Oqv");
+
+      $value = $value / $sensors->$idx;
+
+      discover_sensor($valid['sensor'], 'voltage', $device,
+        $base_oid.$value_index.$idx, $idx, 'sitemonitor', $desc,
+        $sensors->$idx, 1, null, null, null, null, $value);
+
+    }
+
+    break;
+
+  default:
+
+
+  }
