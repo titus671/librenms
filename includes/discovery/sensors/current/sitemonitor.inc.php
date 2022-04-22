9b7e790f93f848dd51712c3cd6b4542c25db4101
Trendal Toews <trendal.toews@gmail.com>
2022-04-22 00:19:07 -0700

    index fix in current/sitemonitor

--- includes/discovery/sensors/current/sitemonitor.inc.php
+++ includes/discovery/sensors/current/sitemonitor.inc.php
@@ -26,12 +26,12 @@
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.1';
 $current = (snmp_get($device, $oid, '-Oqv') / 1);
 $desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.1', '-Oqv'));
-discover_sensor($valid['sensor'], 'current', $device, $oid, 0, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
+discover_sensor($valid['sensor'], 'current', $device, $oid, 1, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
 
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.4';
 $current = (snmp_get($device, $oid, '-Oqv') / 10);
 $desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.4', '-Oqv'));
-discover_sensor($valid['sensor'], 'current', $device, $oid, 0, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
+discover_sensor($valid['sensor'], 'current', $device, $oid, 4, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
 
 
 // Get the expansion unit description, if it exists, if not TODO
