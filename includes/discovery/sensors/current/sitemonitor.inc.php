dfbc866d32c529bc2db50e43d178b6ff29bf2846
Titus671 <titus.friesen@streamitnet.com>
2023-08-28 20:41:39 -0700

    added sitemonitor stuff

--- includes/discovery/sensors/current/sitemonitor.inc.php
+++ includes/discovery/sensors/current/sitemonitor.inc.php
@@ -21,9 +21,85 @@
  *
  * @copyright  2017 Neil Lathwood
  * @author     Neil Lathwood <gh+n@laf.io>
+ * @author     Trendal Toews @ Stream IT Networks
+ *                4-22-2022
+ *                Expansion Modules support and other minor adjustments
  */
+$oid = '.1.3.6.1.4.1.32050.2.1.27.5.1';
+$current = (snmp_get($device, $oid, '-Oqv') / 100);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.1', '-Oqv'));
+discover_sensor($valid['sensor'], 'current', $device, $oid, 1, 'sitemonitor', $desc, 100, 1, null, null, null, null, $current);
+
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.4';
-$current = (snmp_get($device, $oid, '-Oqv') / 10);
-if ($current > 0) {
-    discover_sensor($valid['sensor'], 'current', $device, $oid, 0, 'sitemonitor', 'Current', 10, 1, null, null, null, null, $current);
-}
+$current = (snmp_get($device, $oid, '-Oqv') / 100);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.4', '-Oqv'));
+discover_sensor($valid['sensor'], 'current', $device, $oid, 4, 'sitemonitor', $desc, 100, 1, null, null, null, null, $current);
+
+// Get the expansion unit description, if it exists, if not TODO
+$expansion_module = snmp_get($device, '.1.3.6.1.4.1.32050.2.1.25.2.1', '-Oqv');
+
+switch ($expansion_module) {
+
+  // Run discovery of Morningstar ProStar MPPT Charge Controller
+  case 'ProStar Gen3 RevH':
+
+    $sensors = (object) [
+
+      // divisor mapping
+      '13' => 1000,
+      '15' => 1000,
+      '16' => 1000,
+    ];
+
+    $base_oid = '.1.3.6.1.4.1.32050.2.1.27.';
+    $idx_index = '1.';
+    $desc_index = '2.';
+    $value_index = '5.';
+
+    // $idx will be the sensor index on the Packetflux
+    foreach ($sensors as $idx => $arr) {
+        $index = snmp_get($device, $base_oid . $idx_index . $idx, '-Oqv');
+        $desc = snmp_get($device, $base_oid . $desc_index . $idx, '-Oqv');
+        $value = snmp_get($device, $base_oid . $value_index . $idx, '-Oqv');
+
+        $value = $value / $sensors->$idx;
+
+        discover_sensor($valid['sensor'], 'current', $device,
+        $base_oid . $value_index . $idx, $idx, 'sitemonitor', $desc,
+        $sensors->$idx, 1, null, null, null, null, $value);
+    }
+
+    break;
+
+  case 'TriStarChargeMode':
+
+    $sensors = (object) [
+
+      // divisor mapping
+      '10' => 100,
+      '11' => 100,
+    ];
+
+    $base_oid = '.1.3.6.1.4.1.32050.2.1.27.';
+    $idx_index = '1.';
+    $desc_index = '2.';
+    $value_index = '5.';
+
+    // $idx will be the sensor index on the Packetflux
+    foreach ($sensors as $idx => $arr) {
+        $index = snmp_get($device, $base_oid . $idx_index . $idx, '-Oqv');
+        $desc = snmp_get($device, $base_oid . $desc_index . $idx, '-Oqv');
+        $value = snmp_get($device, $base_oid . $value_index . $idx, '-Oqv');
+
+        $value = $value / $sensors->$idx;
+
+        discover_sensor($valid['sensor'], 'current', $device,
+        $base_oid . $value_index . $idx, $idx, 'sitemonitor', $desc,
+        $sensors->$idx, 1, null, null, null, null, $value);
+    }
+
+    break;
+
+  default:
+
+  }
--- includes/discovery/sensors/voltage/sitemonitor.inc.php
+++ includes/discovery/sensors/voltage/sitemonitor.inc.php
@@ -21,15 +21,146 @@
  *
  * @copyright  2017 Neil Lathwood
  * @author     Neil Lathwood <gh+n@laf.io>
+ * @author     Trendal Toews @ Stream IT Networks
+ *                4-22-2022
+ *                Expansion Modules support and other minor adjustments
  */
-$oid = '.1.3.6.1.4.1.32050.2.1.27.5.1';
-$current = (snmp_get($device, $oid, '-Oqv') / 10);
-discover_sensor($valid['sensor'], 'voltage', $device, $oid, 1, 'sitemonitor', 'Shunt Input', 10, 1, null, null, null, null, $current);
 
+/*
+*   Shunt Input code below moved to the current/sitemonitor.inc.php module
+*/
+//$oid = '.1.3.6.1.4.1.32050.2.1.27.5.1';
+//$current = (snmp_get($device, $oid, '-Oqv') / 10);
+//discover_sensor($valid['sensor'], 'voltage', $device, $oid, 1, 'sitemonitor', 'Shunt Input', 10, 1, null, null, null, null, $current);
+
+/*
+* This is the start of the original code that is left in place to support existing behavior
+*/
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.2';
 $current = (snmp_get($device, $oid, '-Oqv') / 10);
-discover_sensor($valid['sensor'], 'voltage', $device, $oid, 2, 'sitemonitor', 'Power 1', 10, 1, null, null, null, null, $current);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.2', '-Oqv'));
+discover_sensor($valid['sensor'], 'voltage', $device, $oid, 2, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
 
 $oid = '.1.3.6.1.4.1.32050.2.1.27.5.3';
 $current = (snmp_get($device, $oid, '-Oqv') / 10);
-discover_sensor($valid['sensor'], 'voltage', $device, $oid, 3, 'sitemonitor', 'Power 2', 10, 1, null, null, null, null, $current);
+$desc = (snmp_get($device, '.1.3.6.1.4.1.32050.2.1.27.2.3', '-Oqv'));
+discover_sensor($valid['sensor'], 'voltage', $device, $oid, 3, 'sitemonitor', $desc, 10, 1, null, null, null, null, $current);
+
+/*
+* This isthe end of the original code
+*/
+
+
+// Get the expansion unit description, if it exists
+$expansion_module = snmp_get($device, '.1.3.6.1.4.1.32050.2.1.25.2.1', '-Oqv');
+
+switch ($expansion_module) {
+
+  // Run discovery of Morningstar ProStar MPPT Charge Controller
+  case 'ProStar Gen3 RevH':
+
+    $sensors = (object) [
+
+      // divisor mapping
+      '9' => 1000,
+      '10' => 1000,
+      '11' => 1000,
+      '12' => 1000,
+      '14' => 1000,
+    ];
+
+    $base_oid = '.1.3.6.1.4.1.32050.2.1.27.';
+    $idx_index = '1.';
+    $desc_index = '2.';
+    $value_index = '5.';
+
+    // $idx will be the sensor index on the Packetflux
+    foreach ($sensors as $idx => $arr) {
+        $index = snmp_get($device, $base_oid . $idx_index . $idx, '-Oqv');
+        $desc = snmp_get($device, $base_oid . $desc_index . $idx, '-Oqv');
+        $value = snmp_get($device, $base_oid . $value_index . $idx, '-Oqv');
+
+        $value = $value / $sensors->$idx;
+
+        discover_sensor($valid['sensor'], 'voltage', $device,
+        $base_oid . $value_index . $idx, $idx, 'sitemonitor', $desc,
+        $sensors->$idx, 1, null, null, null, null, $value);
+    }
+
+    break;
+
+    // Run discovery of Morningstar TriStar PWM Charge Controller
+    case 'TriStarChargeMode':
+
+      $sensors = (object) [
+
+        // divisor mapping
+        '7' => 100,
+        '8' => 100,
+        '9' => 100,
+        '12' => 100,
+        '23' => 100,
+        '24' => 100,
+        '25' => 100,
+        '28' => 100,
+        '29' => 100,
+      ];
+
+      $base_oid = '.1.3.6.1.4.1.32050.2.1.27.';
+      $idx_index = '1.';
+      $desc_index = '2.';
+      $value_index = '5.';
+
+      // $idx will be the sensor index on the Packetflux
+      foreach ($sensors as $idx => $arr) {
+          $index = snmp_get($device, $base_oid . $idx_index . $idx, '-Oqv');
+          $desc = snmp_get($device, $base_oid . $desc_index . $idx, '-Oqv');
+          $value = snmp_get($device, $base_oid . $value_index . $idx, '-Oqv');
+
+          $value = $value / $sensors->$idx;
+
+          discover_sensor($valid['sensor'], 'voltage', $device,
+          $base_oid . $value_index . $idx, $idx, 'sitemonitor', $desc,
+          $sensors->$idx, 1, null, null, null, null, $value);
+      }
+
+      break;
+
+
+  case '6Voltmeter':
+
+    // Run discovery of the 6 Port Volt Monitoring module
+    $sensors = (object) [
+
+      // divisor mapping
+      '7' => 10,
+      '8' => 10,
+      '9' => 10,
+      '10' => 10,
+      '11' => 10,
+      '12' => 10,
+    ];
+
+    $base_oid = '.1.3.6.1.4.1.32050.2.1.27.';
+    $idx_index = '1.';
+    $desc_index = '2.';
+    $value_index = '5.';
+
+    // $idx will be the sensor index on the Packetflux
+    foreach ($sensors as $idx => $arr) {
+        $index = snmp_get($device, $base_oid . $idx_index . $idx, '-Oqv');
+        $desc = snmp_get($device, $base_oid . $desc_index . $idx, '-Oqv');
+        $value = snmp_get($device, $base_oid . $value_index . $idx, '-Oqv');
+
+        $value = $value / $sensors->$idx;
+
+        discover_sensor($valid['sensor'], 'voltage', $device,
+        $base_oid . $value_index . $idx, $idx, 'sitemonitor', $desc,
+        $sensors->$idx, 1, null, null, null, null, $value);
+    }
+
+    break;
+
+  default:
+
+  }
